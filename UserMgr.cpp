a0ddfa415f37898f28c8edec26fd0dfe7c2b0fba
志诚 李 <1404204769@qq.com>
2021-08-09 19:35:02 +0800

    将玩家登入登出这些与业务有关的部分剥离出去

--- UserMgr.cpp
+++ UserMgr.cpp
@@ -10,81 +10,8 @@ CUserMgr::~CUserMgr() {
 	cout << "调用了CUserMgr的析构函数" << endl;
 	Free();
 }
-bool CUserMgr::AddUser(const string& _strAccount ) {
-	/*将准备新增的用户的数据保存在Map中*/
-	long long int i64Id = getId(_strAccount);
-	if (i64Id != 0) {
-		cout << "登入失败,该用户已登入！" << endl;
-		return false;
-	}
-	mysqlpp::Row row;
-	if (!SearchUser(row, _strAccount)) {
-		cout << "数据库中无此账户" << endl;
-		cout << "新建用户中：\n请输入账户昵称" << endl;
-		string strName;
-		getline(cin, strName);
-		if(!GenerateUser(_strAccount,strName))
-			return false;
-		if (!SearchUser(row, _strAccount)) {
-			cout << "错误：注册成功后数据库依旧无此账户" << endl;
-			return false;
-		}
-	}
-	CUser *pUser=new CUser(_strAccount);
-	if (!pUser) {
-		cout << "创建用户实体失败" << endl;
-		return false;
-	}
-	if (!row) {
-		cout << "错误：没有获取到数据库中的数据返回的Row" << endl;
-		return false;
-	}
-	if (!pUser->InitUser(row)) {
-		cout << "用户初始化失败" << endl;
-		return false;
-	}
-	if (!AddToMap(pUser)) {
-		cout << "用户加入在线队列失败" << endl;
-		return false;
-	}
-	return true;  
-}
 
-
-bool CUserMgr::ReduceUser(const string& _strAccount) {
-	return DeleteFromMap(_strAccount);
-}
-CUser* CUserMgr::getUser(const long long int _i64Id) {
-	/*从map里面查询，返回查询对象指针*/
-	MapByIdIter iterById = m_mapById.find(_i64Id);
-	if (iterById == m_mapById.end()) {
-		cout << "Do not Find ID:" << _i64Id << endl;
-		return nullptr;
-	}
-	return iterById->second;
-}
-long long int CUserMgr::getId(const std::string& _strAccount) {/*如果返回一个0说明该用户不在线*/
-	MapByAccountIter iterByAccount = m_mapByAccount.find(_strAccount);
-	if (iterByAccount == m_mapByAccount.end())return 0;
-	return iterByAccount->second;
-}
-
-/*
-* 以下是对私有容器的操作，不能暴露给外层
-*/
-
-bool CUserMgr::AddToMap(CUser* _pUser) {
-	if (!_pUser)return false;
-	m_mapByAccount[_pUser->getAccount()] = _pUser->getId();
-	m_mapById[_pUser->getId()] = _pUser;
-	if (m_mapByAccount.count(_pUser->getAccount()) == 0 || m_mapById.count(_pUser->getId()) == 0) {
-		cout << "用户登入失败" << endl;
-		ReduceUser(_pUser->getAccount());
-		return false;
-	}
-	return true;
-}
-bool CUserMgr::DeleteFromMap(const string& _strAccount) {
+bool CUserMgr::DelUser(const string& _strAccount) {
 	/*根据Account删除对应的User*/
 	MapByAccountIter iterByAccount = m_mapByAccount.find(_strAccount);
 	if (iterByAccount == m_mapByAccount.end()) {
@@ -103,6 +30,75 @@ bool CUserMgr::DeleteFromMap(const string& _strAccount) {
 	m_mapById.erase(iterById);
 	return true;
 }
+bool CUserMgr::CheckExistInDB(mysqlpp::Row& row, const std::string& _strAccount) {
+	/*检查用户是否存在于数据库*/
+	if (!SearchUser(row, _strAccount)) {
+		cout << "数据库中无此账户" << endl;
+		cout << "新建用户中：\n" << endl;
+		if (!GenerateUser(_strAccount))
+			return false;
+		if (!SearchUser(row, _strAccount)) {
+			cout << "错误：注册成功后数据库依旧无此账户" << endl;
+			return false;
+		}
+	}
+	return true;
+}
+CUser* CUserMgr::getUser(const long long int _i64Id) {
+	/*从map里面查询，返回查询对象指针*/
+	MapByIdIter iterById = m_mapById.find(_i64Id);
+	if (iterById == m_mapById.end()) {
+		cout << "Do not Find ID:" << _i64Id << endl;
+		return nullptr;
+	}
+	return iterById->second;
+}
+long long int CUserMgr::getOnlineUserId(const std::string& _strAccount) {/*如果返回一个0说明该用户不在线*/
+	MapByAccountIter iterByAccount = m_mapByAccount.find(_strAccount);
+	if (iterByAccount == m_mapByAccount.end())return 0;
+	return iterByAccount->second;
+}
+
+void CUserMgr::PrintOnlineUser() {
+	/*打印显示所有在线玩家的信息*/
+	MapByIdIter iterById = m_mapById.begin();
+	while (iterById != m_mapById.end()) {
+		CUser* pUser = iterById->second;
+		iterById++;
+		if (!pUser) {
+			continue;
+		}
+		cout << "ID:" << pUser->getId() << "\tAccount:" << pUser->getAccount() << "\tLev:" << pUser->getLev()<<endl;
+	}
+}
+/*
+* 以下是对私有容器的操作，不能暴露给外层
+*/
+
+bool CUserMgr::AddUser(mysqlpp::Row& row) {
+	CUser* pUser = new CUser("");
+	if (!pUser) {
+		cout << "创建用户实体失败" << endl;
+		return false;
+	}
+	if (!row) {
+		cout << "错误：没有获取到数据库中的数据返回的Row" << endl;
+		return false;
+	}
+	if (!pUser->InitUser(row)) {
+		cout << "用户初始化失败" << endl;
+		return false;
+	}
+	m_mapByAccount[pUser->getAccount()] = pUser->getId();
+	m_mapById[pUser->getId()] = pUser;
+	if (m_mapByAccount.count(pUser->getAccount()) == 0 || m_mapById.count(pUser->getId()) == 0) {
+		cout << "用户登入未生效" << endl;
+		DelUser(pUser->getAccount());
+		return false;
+	}
+	return true;
+}
+
 void CUserMgr::Free() {
 	/*在析构函数中调用，释放还在内存中的数据，防止内存泄漏以及数据丢失*/
 	MapByAccountIter iterByAccount = m_mapByAccount.begin();
@@ -179,9 +175,9 @@ bool CUserMgr::SearchUser(mysqlpp::Row& row, const std::string& _strAccount) {
 	return true;
 }
 
-bool CUserMgr::GenerateUser(const string& _strAccount, const string& _strName) {
+bool CUserMgr::GenerateUser(const string& _strAccount) {
 	/*参数为account，name,调用CUser的InsertUser*/
-	CUser user(_strAccount, _strName);
+	CUser user(_strAccount);
 	if (!user.InsertUser()) {
 		cout << "用户注册失败" << endl;
 		return false;
