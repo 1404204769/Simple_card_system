a0cee93778e36c709be33591a228fcad70f16ff3
志诚 李 <1404204769@qq.com>
2021-08-11 14:07:51 +0800

    静态卡牌配置数据

--- CardType.cpp
+++ CardType.cpp
@@ -4,13 +4,15 @@ CCardType::CCardType() {
 	/*构造函数*/
 	stringstream strIn;
 	strIn << "调用了CCardType构造函数\n";
-	OutputDebugPrintf(strIn.str().c_str());
+	string strInput(strIn.str());
+	OutputDebugPrintf(strInput.c_str());
 }
 CCardType::~CCardType() {
 	/*析构函数*/
 	stringstream strIn;
 	strIn << "调用了CCardType析构函数\n";
-	OutputDebugPrintf(strIn.str().c_str());
+	string strInput(strIn.str());
+	OutputDebugPrintf(strInput.c_str());
 }
 bool CCardType::Init(mysqlpp::Row& row) {
 	/*根据获取的数据库数据初始化静态卡牌数据*/
@@ -31,50 +33,54 @@ bool CCardType::Init(mysqlpp::Row& row) {
 	catch (const mysqlpp::BadQuery& er) {
 		stringstream strIn;
 		strIn << "CCardType::Init()\nQuery error: " << er.what() << "\n";
-		OutputDebugPrintf(strIn.str().c_str());
+		string strInput(strIn.str());
+		OutputDebugPrintf(strInput.c_str());
 		return false;
 	}
 	catch (const mysqlpp::BadConversion& er) {
 		stringstream strIn;
 		strIn << "CCardType::Init()\nConversion error: " << er.what() << "\ntretrieved data size: " << er.retrieved << ", actual size: " << er.actual_size << "\n";
-		OutputDebugPrintf(strIn.str().c_str());
+		string strInput(strIn.str());
+		OutputDebugPrintf(strInput.c_str());
 		return false;
 	}
 	catch (const mysqlpp::BadIndex& er) {
 		stringstream strIn;
 		strIn << "CCardType::Init()\nError: " << er.what() << "\n";
-		OutputDebugPrintf(strIn.str().c_str());
+		string strInput(strIn.str());
+		OutputDebugPrintf(strInput.c_str());
 		return false;
 	}
 	catch (const mysqlpp::Exception& er) {
 		stringstream strIn;
 		strIn << "CCardType::Init()\nError: " << er.what() << "\n";
-		OutputDebugPrintf(strIn.str().c_str());
+		string strInput(strIn.str());
+		OutputDebugPrintf(strInput.c_str());
 		return false;
 	}
 	return true;
 }
-const std::string& CCardType::getName() const {
+const std::string& CCardType::GetName() const {
 	/*获取m_strName*/
 	return m_strName;
 }
-unsigned int CCardType::getId() const {
+unsigned int CCardType::GetId() const {
 	/*获取m_unId*/
 	return m_unId;
 }
-unsigned int CCardType::getCardType() const {
+unsigned int CCardType::GetCardType() const {
 	/*获取m_unCardType*/
 	return m_unCardType;
 }
-long long int CCardType::getHp() const {
+long long int CCardType::GetHp() const {
 	/*获取m_i64Hp*/
 	return m_i64Hp;
 }
-long long int CCardType::getMp() const {
+long long int CCardType::GetMp() const {
 	/*获取m_i64Mp*/
 	return m_i64Mp;
 }
-long long int CCardType::getAtk() const {
+long long int CCardType::GetAtk() const {
 	/*获取m_i64Atk*/
 	return m_i64Atk;
 }
--- CardType.h
+++ CardType.h
@@ -11,12 +11,12 @@ public:
 	CCardType();/*构造函数*/
 	~CCardType();/*析构函数*/
 	bool Init(mysqlpp::Row& row);/*根据获取的数据库数据初始化静态卡牌数据*/
-	const std::string& getName() const;/*获取m_strName*/
-	unsigned int getId() const;/*获取m_unId*/
-	unsigned int getCardType() const;/*获取m_unCardType*/
-	long long int getHp() const;/*获取m_i64Hp*/
-	long long int getMp() const;/*获取m_i64Mp*/
-	long long int getAtk() const;/*获取m_i64Atk*/
+	const std::string& GetName() const;/*获取m_strName*/
+	unsigned int GetId() const;/*获取m_unId*/
+	unsigned int GetCardType() const;/*获取m_unCardType*/
+	long long int GetHp() const;/*获取m_i64Hp*/
+	long long int GetMp() const;/*获取m_i64Mp*/
+	long long int GetAtk() const;/*获取m_i64Atk*/
 private:
 	unsigned int m_unId=0,m_unCardType=0;
 	std::string m_strName="";
